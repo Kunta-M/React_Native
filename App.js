360f0c366d62857431fe16ce561f194769f0c20f
Kunta-M <mariia.kunta@gmail.com>
2021-09-12 17:50:31 +0300

    clean master

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Posts from "./components/Posts";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Posts/>
-
     </View>
   );
 }
