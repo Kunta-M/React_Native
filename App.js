4e5e40511f597929454207deeb995f452b5056a0
Kunta-M <mariia.kunta@gmail.com>
2021-09-13 22:07:09 +0300

    lesson2

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Users from "./components/User";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+      <Users/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-
-  },
+  container: {},
 });
